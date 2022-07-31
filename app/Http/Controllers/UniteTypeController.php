d18f79ef503c79c8cf5dfb8ff4aac51ec2562831
rayhan-rimon <rayhanrimon17@gmail.com>
2022-07-31 10:23:01 +0600

    Create unitType & UnitTypeController database addes

--- app/Http/Controllers/UniteTypeController.php
+++ app/Http/Controllers/UniteTypeController.php
@@ -4,8 +4,83 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\uniteType;
 
 class UniteTypeController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\unitType  $adminCostType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(uniteType $uniteType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\unitType  $adminCostType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(uniteType $uniteType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\adminCostType  $adminCostType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, uniteType $uniteType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\uniteType  $adminCostType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(uniteType $uniteType)
+    {
+        //
+    }
 }
--- app/Models/uniteType.php
+++ app/Models/uniteType.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class uniteType extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'uniteName',
+    ];
 }
