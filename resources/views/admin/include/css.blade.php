76b080053f8330a2b6390b2ae4e19ba07832feaf
Nayeem <mohtamimnayeem@gmail.com>
2022-07-31 16:12:05 +0600

    Updated

---
+++ resources/views/admin/include/css.blade.php
@@ -0,0 +1,14 @@
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<!-- VENDOR CSS -->
+<link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/vendor/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="assets/vendor/animate-css/vivify.min.css">
+
+<link rel="stylesheet" href="assets/vendor/chartist/css/chartist.min.css">
+<link rel="stylesheet" href="assets/vendor/chartist-plugin-tooltip/chartist-plugin-tooltip.css">
+<link rel="stylesheet" href="assets/vendor/c3/c3.min.css"/>
+<link rel="stylesheet" href="assets/vendor/toastr/toastr.min.css">
+<link rel="stylesheet" href="assets/vendor/jvectormap/jquery-jvectormap-2.0.3.min.css"/>
+
+<!-- MAIN CSS -->
+<link rel="stylesheet" href="assets/css/mooli.min.css">
