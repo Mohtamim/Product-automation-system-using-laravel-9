ecd43bcacdbb2a8c821726cef39cf93172530db4
Nayeem <mohtamimnayeem@gmail.com>
2022-07-31 16:25:57 +0600

    Updated

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Product automation System</title>
+    @include('admin.include.css')
 </head>
 <body>
 <div>
     @yield('mainPart')
 </div>
+@include('admin.include.js')
 </body>
 </html>
