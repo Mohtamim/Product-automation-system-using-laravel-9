dd8cd6268d242e9e84f6c3584bad51e1fbaefa37
Nayeem <mohtamimnayeem@gmail.com>
2022-07-31 16:15:33 +0600

    Updated

--- resources/views/admin/include/css.blade.php
+++ resources/views/admin/include/css.blade.php
@@ -1,4 +1,4 @@
-<link rel="icon" href="favicon.ico" type="image/x-icon">
+<link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 <!-- VENDOR CSS -->
 <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" href="assets/vendor/font-awesome/css/font-awesome.min.css">
