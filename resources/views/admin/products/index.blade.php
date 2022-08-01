d45709e4d19dff4346cfb7940fdd5600364273c3
Mosarof Hossain <mosarof.hossain.cse@gmail.com>
2022-08-01 16:12:25 +0600

    Products Crud Completed

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,5 +1,5 @@
-@extends('products.layout')
-@section('content')
+@extends('admin.include.layout')
+@section('mainPart')
     <div class="container">
         <div class="row bg-red" style="margin:20px;">
             <div class="col-12">
@@ -7,9 +7,9 @@
                     <div class="card-header">
                         <h2>Product List</h2>
                     </div>
-
+   
                     <div class="card-body">
-                        <a href="{{url('students/create')}}" class="btn btn-success btn-sm" title="Add new Student">
+                         <a href="{{url('admin/products/create')}}" class="btn btn-success btn-sm" title="Add new Student">
                             Add New
                         </a><br><br>
                         @if (session('flash_message'))
@@ -42,9 +42,9 @@
                                         <td>{{$item->productPrice}}</td>
                                         <td>{{$item->updatedStock}}</td>
                                         <td>
-                                            <a href="{{url('//'.$item->id)}}" class="btn btn-info btn-sm" title="View Student" aria-hidden="true"><i class="fa fa-eye"></i></a>
-                                            <a href="{{url('//'.$item->id.'/edit')}}" class="btn btn-primary btn-sm" title="Edit Student" aria-hidden="true"><i class="fa fa-pencil"></i></a>
-                                            <form method="POST" action="{{ url('/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
+                                            <a href="{{url('admin/products/'.$item->id)}}" class="btn btn-info btn-sm" title="View Student" aria-hidden="true"><i class="fa fa-eye"></i></a>
+                                            <a href="{{url('admin/products/'.$item->id.'/edit')}}" class="btn btn-primary btn-sm" title="Edit Student" aria-hidden="true"><i class="fa fa-pencil"></i></a>
+                                            <form method="POST" action="{{ url('admin/products/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
                                                 {{ method_field('DELETE') }}
                                                 {{ csrf_field() }}
                                                 <button type="submit" class="btn btn-danger btn-sm" title="Delete Student" onclick="return confirm("Confirm delete?")"><i class="fa fa-trash" aria-hidden="true"></i></button>
